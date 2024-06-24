9149118388b5c4db81b58a833f47bf175e955869
Rikuto <orikutobasuke@gmail.com>
2024-06-24 14:14:11 +0900

    ハンバーガーメニュー動き完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,15 @@ import Image from "next/image";
 import styles from "@/styles/page.module.scss";
 
 import Button from '@/components/Button'
+import Header from '@/components/Header'
 
 export default function Home() {
   return (
     <>
-      <Button text="詳しく見る" />
+      <div>
+        <Header />
+        <Button text="詳しく見る" />
+      </div>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,26 @@
 'use client'
 
+import { useState, useEffect } from 'react'
+
 import style from '@/styles/components/header.module.scss'
 
 export default function Header() {
+
+    const [menuFlg, setMenuFlg] = useState<boolean>(false)
 
     return (
         <>
             <header className={style.headerWrap}>
                 <figure>
-                    <img src="img/ECCCompDesignLogoType.svg" alt="" />
+                    <img src="/img/quickwork.svg" alt="ロゴ" />
                 </figure>
+                <div
+                    className={style.hubergerMenu}
+                    onClick={() => setMenuFlg(!menuFlg)}
+                >
+                    <span className={!menuFlg ? style.menuLineTop : style.menuLineTopAfter}></span>
+                    <span className={!menuFlg ? style.menuLineBottom : style.menuLineBottomAfter}></span>
+                </div>
             </header>
         </>
     )
