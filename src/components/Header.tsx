f43abd55668bf0a4a0c73eef4d795f794493e7f2
Rikuto <orikutobasuke@gmail.com>
2024-06-24 16:40:16 +0900

    ハンバーガーメニュー完成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,6 +21,31 @@ export default function Header() {
                     <span className={!menuFlg ? style.menuLineTop : style.menuLineTopAfter}></span>
                     <span className={!menuFlg ? style.menuLineBottom : style.menuLineBottomAfter}></span>
                 </div>
+                <div className={!menuFlg ? style.backColor : style.backColorAfter}></div>
+                <nav className={!menuFlg ? style.menuArea : style.menuAreaAfter}>
+                    <ul className={style.menuList}>
+                        <li className={style.menulistItem}>
+                            <ruby>新たな景色を
+                                <rp>(</rp><rt>Use scene</rt><rp>)</rp>
+                            </ruby>
+                        </li>
+                        <li className={style.menulistItem}>
+                            <ruby>そのスピードで
+                                <rp>(</rp><rt>Utility</rt><rp>)</rp>
+                            </ruby>
+                        </li>
+                        <li className={style.menulistItem}>
+                            <ruby>鮮やかに彩り
+                                <rp>(</rp><rt>Design</rt><rp>)</rp>
+                            </ruby>
+                        </li>
+                        <li className={style.menulistItem}>
+                            <ruby>あなたの職場へ
+                                <rp>(</rp><rt>Rent Quick work</rt><rp>)</rp>
+                            </ruby>
+                        </li>
+                    </ul>
+                </nav>
             </header>
         </>
     )
