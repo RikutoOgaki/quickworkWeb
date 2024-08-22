4fddd7306fd6b79e393231094a6ee657c33d5a14
Rikuto <orikutobasuke@gmail.com>
2024-08-22 09:41:50 +0900

    再作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,6 @@ import style from "@/styles/page.module.scss";
 import Button from '@/components/template/Button'
 import Header from '@/components/template/Header'
 import ScrollEffect from "@/components/template/ScrollEffect";
-import MainView from '@/components/common/MainView'
-import Overview from "@/components/common/Overview";
-import MovieContent from "@/components/common/MovieContent";
-import Performance from "@/components/common/Performance";
-import Slider from "@/components/common/Slider";
-import Experience from "@/components/common/Experience";
 import Footer from '@/components/template/Footer'
 
 export default function Home() {
@@ -18,13 +12,7 @@ export default function Home() {
     <>
       <div className={style.quickWorkWrap}>
         <Header />
-        <MainView />
         <ScrollEffect />
-        <Overview />
-        <MovieContent />
-        <Performance />
-        <Slider />
-        <Experience />
         <Footer />
       </div>
     </>
